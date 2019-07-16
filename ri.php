2e62e73c1a35daa4e72e284c02e21fa2ef84a43f
grayVTouch <A576236148946@126.com>
2019-07-16 16:37:32 +0800

    by cxl for local +3

--- ri.php
+++ ri.php
@@ -3,7 +3,9 @@
  * Created by PhpStorm.
  * User: grayVTouch
  * Date: 2019/7/16
- * Time: 16:29
+ * Time: 16:36
  */
-echo 'nihao';
-echo 'cao';
+
+
+
+echo 'ri';
--- test.php
+++ test.php
@@ -7,3 +7,4 @@
  */
 
 echo 'hello fuck';
+echo 'caodan';
